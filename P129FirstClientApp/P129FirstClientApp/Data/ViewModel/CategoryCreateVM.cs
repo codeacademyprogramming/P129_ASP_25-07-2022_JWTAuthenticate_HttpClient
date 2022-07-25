4adbaca9a2e6b50c18f2bbe1a261a048711ae9f3
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-07-25 12:44:25 +0400

    First Commit

---
+++ P129FirstClientApp/P129FirstClientApp/Data/ViewModel/CategoryCreateVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P129FirstClientApp.Data.ViewModel
+{
+    public class CategoryCreateVM
+    {
+        public string Ad { get; set; }
+        public bool Esasdirmi { get; set; }
+        public Nullable<int> AidOlduguUstCategoryId { get; set; }
+        public string Sekli { get; set; }
+    }
+}
